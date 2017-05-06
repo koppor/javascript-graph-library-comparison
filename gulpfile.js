fad4cd5ccf2a3187645efe8dc8d73b578bcf9ca7
Alex Frank <alex_frank@email.com>
2017-05-06 17:48:58 +0200

    Added basic index html

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 var gulp = require('gulp');
-var inject = require('gulp-inject');
-var mainBowerFiles = require('main-bower-files');
 var browserSync = require('browser-sync').create();
-var es = require('event-stream');
 
 /**
  * Use BrowserSync for livereload
@@ -17,17 +14,5 @@ gulp.task('browser-sync', function () {
     gulp.watch('src/**').on('change', browserSync.reload)
 });
 
-/**
- * Task that injects all the bower components we need
- */
-gulp.task('index', function () {
-    gulp.src('./src/index.html')
-        .pipe(inject(gulp.src(mainBowerFiles(), { read: false }), { name: 'bower', relative: true }))
-        .pipe(gulp.dest('./src'))
-        .pipe(inject(es.merge(
-            gulp.src('./src/app/**/*.js', { read: false })
-        )))
-        .pipe(gulp.dest('./src'));
-});
 
-gulp.task('default', ['index', 'browser-sync']);
+gulp.task('default', ['browser-sync']);
