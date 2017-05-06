22864bc42cd88d8ffaab9fda8bb8a24e37a5ba8a
Alex Frank <alex_frank@email.com>
2017-05-06 15:36:25 +0200

    Added browersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,35 @@ var inject = require('gulp-inject');
 var mainBowerFiles = require('main-bower-files');
 var beautify = require('gulp-beautify');
 var htmlbeautify = require('gulp-html-beautify');
+var browserSync = require('browser-sync').create();
 
-gulp.task('htmlbeautify', function() {
+/**
+ * Use BrowserSync for livereload
+ */
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./src"
+        }
+    });
+
+    gulp.watch('src/**').on('change',browserSync.reload)
+});
+/**
+ * Beautifies html
+ */
+gulp.task('htmlbeautify', function () {
   gulp.src('./src/**/*.html')
-    .pipe(htmlbeautify({indent_size: 2}))
+    .pipe(htmlbeautify({ indent_size: 2 }))
     .pipe(gulp.dest('./src/'))
 });
 
 /**
  * Beautifies javascript files, because we dont need them minified
  */
-gulp.task('jsbeautify', function() {
+gulp.task('jsbeautify', function () {
   gulp.src('./src/**/*.js')
-    .pipe(beautify({indent_size: 2}))
+    .pipe(beautify({ indent_size: 2 }))
     .pipe(gulp.dest('./src/'))
 });
 
@@ -24,9 +40,9 @@ gulp.task('jsbeautify', function() {
  */
 gulp.task('index', function () {
   console.log(mainBowerFiles());
-    return gulp.src('./src/index.html')
-        .pipe(inject(gulp.src(mainBowerFiles(), { read: false }), { name: 'bower', relative: true }))
-        .pipe(gulp.dest('./src'));
+  return gulp.src('./src/index.html')
+    .pipe(inject(gulp.src(mainBowerFiles(), { read: false }), { name: 'bower', relative: true }))
+    .pipe(gulp.dest('./src'));
 });
 
-gulp.task('default', ['index', 'jsbeautify', 'htmlbeautify']);
+gulp.task('default', ['index', 'jsbeautify', 'htmlbeautify', 'browser-sync']);
