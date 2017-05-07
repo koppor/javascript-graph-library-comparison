7ab2eca64fac556f09f2e3401ee828f4fb679a44
Alex Frank <alex_frank@email.com>
2017-05-07 14:21:20 +0200

    Added raphael basic project

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-
+var mainBowerFiles = require('main-bower-files');
+var inject = require('gulp-inject');
+var es = require('event-stream');
 /**
  * Use BrowserSync for livereload
  */
@@ -14,5 +16,18 @@ gulp.task('browser-sync', function () {
     gulp.watch('src/**').on('change', browserSync.reload)
 });
 
+/**
+ * Gulp injection
+ */
+gulp.task('index', function () {
+    gulp.src('./src/index.html')
+        .pipe(inject(gulp.src(mainBowerFiles(), { read: false }), { name: 'bower', relative: true }))
+        .pipe(gulp.dest('./src'))
+        .pipe(inject(es.merge(
+            gulp.src('./src/app/*.js', { read: false })
+        )))
+        .pipe(gulp.dest('./src'));
+});
 
-gulp.task('default', ['browser-sync']);
+
+gulp.task('default', ['index', 'browser-sync']);
