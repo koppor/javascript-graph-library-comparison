4b35bbe56ed44e7ce57d74e304fbc29d39e8e4cf
Alex Frank <alex_frank@email.com>
2017-05-07 14:22:33 +0200

    Added relativ path

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var mainBowerFiles = require('main-bower-files');
 var inject = require('gulp-inject');
-var es = require('event-stream');
 /**
  * Use BrowserSync for livereload
  */
@@ -23,9 +22,7 @@ gulp.task('index', function () {
     gulp.src('./src/index.html')
         .pipe(inject(gulp.src(mainBowerFiles(), { read: false }), { name: 'bower', relative: true }))
         .pipe(gulp.dest('./src'))
-        .pipe(inject(es.merge(
-            gulp.src('./src/app/*.js', { read: false })
-        )))
+        .pipe(inject(gulp.src('./src/app/*.js', { read: false }), { relative: true }))
         .pipe(gulp.dest('./src'));
 });
 
