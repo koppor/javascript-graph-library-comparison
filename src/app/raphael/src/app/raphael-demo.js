6ed03290399eb6f2e9772b45bb7b91e5f73ff158
Alex Frank <alex_frank@email.com>
2017-05-07 16:06:25 +0200

    Finished basic example of raphaeljs

--- src/app/raphael/src/app/raphael-demo.js
+++ src/app/raphael/src/app/raphael-demo.js
@@ -1,10 +1,49 @@
-// Creates canvas 320 × 200 at 10, 50
-var paper = Raphael(10, 50, 320, 200);
 
-// Creates circle at x = 50, y = 40, with radius 10
-var circle = paper.circle(50, 40, 10);
-// Sets the fill attribute of the circle to red (#f00)
-circle.attr("fill", "#f00");
+(function () {
+    "use strict";
+    Raphael.fn.arrow = function (x1, y1, x2, y2, size) {
+        var angle = Raphael.angle(x1, y1, x2, y2);
+        var a45 = Raphael.rad(angle - 45);
+        var a45m = Raphael.rad(angle + 45);
+        var x2a = x2 + Math.cos(a45) * size;
+        var y2a = y2 + Math.sin(a45) * size;
+        var x2b = x2 + Math.cos(a45m) * size;
+        var y2b = y2 + Math.sin(a45m) * size;
+        return this.path(
+            "M" + x1 + " " + y1 + "L" + x2 + " " + y2 +
+            "M" + x2 + " " + y2 + "L" + x2a + " " + y2a +
+            "M" + x2 + " " + y2 + "L" + x2b + " " + y2b
+        );
+    };
 
-// Sets the stroke attribute of the circle to white
-circle.attr("stroke", "#fff");
+    var paper = Raphael("canvas");
+
+    initCircles();
+    initRects();
+
+    function initCircles() {
+        var circle1 = paper.circle(30, 40, 10);
+        var circle2 = paper.circle(440, 40, 10)
+        var arrow = paper.arrow(30 + 10, 40, 440 - 10, 40, 5);
+        var text = paper.text(195, 35, "Text")
+        var circleText = paper.text(30, 40, "C")
+        circle1.attr("fill", "#0f0");
+        circle1.attr("stroke", "#fff");
+        arrow.attr("stroke", "#f00");
+        arrow.attr("stroke-dasharray", "-");
+    }
+
+    function initRects() {
+        var rect1 = paper.rect(30, 440, 10, 10)
+        var rect2 = paper.rect(440, 440, 10, 10)
+        var arrow = paper.arrow(30 + 10, 440 + 5, 440, 440 + 5, 5);
+
+        arrow.attr("stroke", "#00f");
+        arrow.attr("stroke-dasharray", ".");
+        rect1.attr("fill", "#00f");
+        rect1.attr("stroke", "#0FF");
+        rect2.attr("stroke-dasharray", ".")
+    }
+
+})();
+
