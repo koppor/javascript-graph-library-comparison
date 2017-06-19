3364df822427cb476f3c644cb35676618fbf9585
Franziska Hutter <franziska_hutter@Ok.de>
2017-06-19 21:57:09 +0200

    JointJS
    JointJs

--- src/app/jointjs/src/app/jointjs-demo.js
+++ src/app/jointjs/src/app/jointjs-demo.js
@@ -4,85 +4,151 @@
 (function () {
     "use strict";
 
-    var graph = new joint.dia.Graph;
-    var graph2 = new joint.dia.Graph;
+    var canvas = $('#canvas');
+    var graph = new joint.dia.Graph();
+
+
+    var link2 = new joint.dia.Link({
+        el: $('#link'),
+    });
+
     var paper = new joint.dia.Paper({
-        el: $('#myholder'),
-        width: 500,
-        height: 500,
+        el: canvas,
+        width: canvas.outerWidth(),
+        height: canvas.outerHeight(),
         model: graph,
-        gridSize: 1
+        defaultLink: new joint.dia.Link({
+            attrs: { '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' }}
+        }),
     });
-    graph.on('all', function(eventName, cell) {
-       // console.log(arguments);
-    });
-    var paper2 = new joint.dia.Paper({
-        el: $('#myholder2'),
-        width: 500,
-        height: 500,
-        model: graph2,
-        gridSize: 1
-    });
-    graph2.on('all', function(eventName, cell) {
-       // console.log(arguments);
-    });
-    var rect = new joint.shapes.basic.Rect({
+
+
+    var rect = new joint.shapes.devs.Model({
         position: { x: 10, y: 50 },
         size: { width: 100, height: 80 },
-        attrs: { rect: { fill: '#2C3E50' }, text: { text: 'Rect', fill: 'white' } },
+        attrs: {
+            '.label': { text: 'Model', 'ref-x': .5, 'ref-y': .2 },
+            rect: { fill: '#2C3E50'  }
+        }
+
     });
-    var circle = new joint.shapes.basic.Circle({
+
+    joint.shapes.devs.CircleModel = joint.shapes.devs.Model.extend({
+
+        markup: '<g class="rotatable"><g class="scalable"><circle class="body"/></g><text class="label"/><g class="inPorts"/><g class="outPorts"/></g>',
+        portMarkup: '<g class="port port<%= id %>"><rect class="port-body"/><text class="port-label"/></g>',
+
+        defaults: joint.util.deepSupplement({
+            type: 'devs.CircleModel',
+            attrs: {
+                '.body': { r: 50, cx: 50 },
+                '.label': { text: 'Circle Model', 'ref-y': 0.5, 'y-alignment': 'middle' },
+                '.port-body': { width: 10, height: 10, x: -5, stroke: 'gray', fill: 'lightgray', magnet: 'active' }
+            }
+
+        }, joint.shapes.devs.Model.prototype.defaults)
+    });
+
+    joint.shapes.devs.CircleModelView = joint.shapes.devs.ModelView;
+        var circle = new joint.shapes.devs.CircleModel({
         position: { x: 10, y: 400 },
         size: { width: 100, height: 80 },
-        attrs: { rect: { fill: 'white' }, text: { text: 'Circle', fill: 'black' } }
+        attrs: { circle: { fill: 'white' }, text: { text: 'Circle', fill: 'black' } }
     });
 
 
-    var rect2 = rect.clone();
-    rect2.translate(300);
+    function out(m) {
+        $('#paper-link-out').html(m);
+    }
 
-    rect2.attr({
-        rect: { fill: '#2C3E50', rx: 5, ry: 5, 'stroke-width': 2, stroke: 'black', 'stroke-dasharray': '2,5' },
+
+    var rect2 = new joint.shapes.devs.Model({
+        type: 'popup.Element',
+        position: { x: 310, y: 50 },
+        size: { width: 100, height: 80 },
+
+        attrs: {
+            '.label': { text: 'Model', 'ref-x': .5, 'ref-y': .2 },
+            rect: { rx: 5, ry: 5, stroke: 'black', 'stroke-dasharray': '2,5'}
+        },
+
     });
 
 
-    var circle2 = circle.clone();
-    circle2.translate(300);
+    var circle2 = new joint.shapes.devs.CircleModel({
+        position: { x: 310, y: 400 },
+        size: { width: 100, height: 80 },
+        attrs: { circle: { fill: 'white' }, text: { text: 'Circle', fill: 'black' } }
+    });
+
     rect.on('change:position', function(element) {
     //    console.log(element.id, ':', element.get('position'));
     });
     circle.on('change:position', function(element) {
       //  console.log(element.id, ':', element.get('position'));
     });
-    var link = new joint.dia.Link({
 
-        source: { id: rect.id },
-        target: { id: rect2.id },
-        labels : [ {
-            position : .5,
-            attrs : {
-                text : {
-                    text : 'LinkLabel',
-                    fill : 'gray'
-                }
+
+    graph.addCells([rect, rect2]);
+    graph.addCells([circle, circle2]);
+
+    graph.on('change:source change:target', function(link) {
+        if (link.get('source').id && link.get('target').id) {
+            // both ends of the link are connected.
+        }
+    });
+
+
+
+
+    paper.on('cell:pointerdblclick', function(cellView, evt, x, y) {
+
+
+        var portsArray = cellView.model.get('outPorts') || [];
+        cellView.model.set('outPorts', portsArray.concat([name]), cellView.opt);
+
+
+
+     //   var portsArray = cellView.model.get('outPorts') || [];
+       // cellView.model.set('outPorts', portsArray.concat([name]), cellView.opt);
+
+                   //  "<div><button class='btn btn-block' id='" + btnDashedId + "'>Dashed</button>" +
+                     //   "<button class='btn btn-block' id='" + btnSolidId + "'>Solid</button></div>";
+
+    });
+
+
+    document.getElementById('addRectBtn').onclick= function() {
+
+
+        var rec=  new joint.shapes.devs.Model({
+            position: { x: 10, y: 10 },
+            size: { width: 100, height: 80 },
+            attrs: {
+                '.label': { text: 'Model' },
+                rect: { fill: '#2C3E50', rx: 5, ry: 5, 'stroke-width': 2, stroke: 'black'}
             }
-        } ]
-    });
-    link.attr({
-        '.connection': { stroke: 'red', text: 'text' },
-        '.marker-target': { fill: 'red', d: 'M 10 0 L 0 5 L 10 10 z' },
 
-    });
-    var link2 = new joint.dia.Link({
-        el: $('#link'),
-        source: { id: circle.id },
-        target: { id: circle2.id }
-    });
-    link2.attr({
-        '.connection': { 'stroke-dasharray': '2,5'},
-        '.marker-source': { fill: 'black', d: 'M 10 0 L 0 5 L 10 10 z' },
-    });
-    graph.addCells([rect, rect2, link]);
-    graph.addCells([circle, circle2, link2]);
+        });
+
+        graph.addCells([rec]);
+
+    };
+
+
+    document.getElementById('addCircleBtn').onclick= function() {
+
+        var circle2 = new joint.shapes.devs.CircleModel({
+
+            position: { x: 10, y: 10 },
+            size: { width: 100, height: 80 },
+            attrs: { circle: { fill: 'white' }, text: { text: 'Circle', fill: 'black' } }
+        });
+
+        graph.addCells([circle2]);
+
+    };
+
 
 }());
+
