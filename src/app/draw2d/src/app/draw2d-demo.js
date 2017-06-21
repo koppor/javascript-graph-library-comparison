9a7fe219761b0f19f3617c8905c00ea9eb4e7d95
Birasanth Pushpanathan <birasanth.push@hotmail.de>
2017-06-21 12:17:48 +0200

    bower added

---
+++ src/app/draw2d/src/app/draw2d-demo.js
@@ -0,0 +1,88 @@
+(function () {
+    "use strict";
+
+    //Libraries
+    var paper = Raphael("canvas");
+    var watch = WatchJS.watch;
+
+    /**
+     * Creates an arrow
+     * @param source The target element
+     * @param destination The source element
+     */
+
+
+    /**
+     * Center function
+     * @returns {*}
+     */
+
+
+    /**
+     * Adds text to the element
+     * @param text
+     */
+
+
+    /**
+     * Makes an element draggable
+     */
+
+
+    /**
+     * Sets the html color
+     * @param htmlColor The html color
+     */
+
+
+    /**
+     * Sets the stroke color
+     * @param strokeColor The stroke color
+     */
+
+
+    /**
+     * Sets the stroke style
+     * @param strokeStyle The stroke style
+     */
+
+
+    /**
+     * Adds a popover to the element
+     */
+
+
+    /**
+     * Adds a Rectangle
+     */
+
+
+    /**
+     * <canvas id="canvas" width="800px" height="600px"></canvas>nts the rectangles
+     */
+
+
+    /**
+     * Adds a circle
+     */
+
+
+    /**
+     * Inits the circles
+     */
+
+
+    /**
+     * Main entry method
+     */
+    function main() {
+        initRectangles();
+        initCircles();
+
+
+    }
+
+    main();
+
+})();
+
