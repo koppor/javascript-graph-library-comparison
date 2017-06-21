29e9559c7ead1e92fff4206b17e911b6b4ffc874
Alex Frank <alex_frank@email.com>
2017-06-21 12:48:50 +0200

    Code refactoring to meet the guidelines

--- src/app/jointjs/src/app/jointjs-demo.js
+++ src/app/jointjs/src/app/jointjs-demo.js
@@ -5,150 +5,132 @@
     "use strict";
 
     var graph = new joint.dia.Graph();
-
-
-    var link2 = new joint.dia.Link({
-        el: $('#link'),
-        text: 'Label'
-    });
-
     var paper = new joint.dia.Paper({
         el: $('#canvas'),
         width: $('#canvas').outerWidth(),
         height: $('#canvas').outerHeight(),
         model: graph,
         defaultLink: new joint.dia.Link({
-            attrs: { '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' },
-            text: 'label'}
+            attrs: {
+                '.marker-target': {d: 'M 10 0 L 0 5 L 10 10 z'}
+            }
         })
     });
 
 
-    var rect = new joint.shapes.devs.Model({
-        position: { x: 10, y: 50 },
-        size: { width: 100, height: 80 },
-        attrs: {
-            '.label': { text: 'Model'}
-        }
-
-    });
-
-    joint.shapes.devs.CircleModel = joint.shapes.devs.Model.extend({
-
-        markup: '<g class="rotatable"><g class="scalable"><circle class="body"/></g><text class="label"/><g class="inPorts"/><g class="outPorts"/></g>',
-        portMarkup: '<g class="port port<%= id %>"><rect class="port-body"/><text class="port-label"/></g>',
-
-        defaults: joint.util.deepSupplement({
-            type: 'devs.CircleModel',
-            attrs: {
-                '.body': { r: 50, cx: 50 },
-                '.label': { text: 'Circle Model', 'ref-y': 0.5, 'y-alignment': 'middle' },
-                '.port-body': { width: 10, height: 10, x: -5, stroke: 'gray', fill: 'lightgray', magnet: 'active' }
-            }
-
-        }, joint.shapes.devs.Model.prototype.defaults)
-    });
-
-    joint.shapes.devs.CircleModelView = joint.shapes.devs.ModelView;
-        var circle = new joint.shapes.devs.CircleModel({
-        position: { x: 10, y: 400 },
-        size: { width: 100, height: 80 },
-        attrs: { circle: { fill: 'white' }, text: { text: 'Circle', fill: 'black' } }
-});
-
-
-    function out(m) {
-        $('#paper-link-out').html(m);
-    }
-
-
-    var rect2 = new joint.shapes.devs.Model({
-        position: { x: 310, y: 50 },
-        size: { width: 100, height: 80 },
-
-        attrs: {
-            '.label': { text: 'Model', 'ref-x': .5, 'ref-y': .2 },
-            rect: { rx: 5, ry: 5, stroke: 'black', 'stroke-dasharray': '2,5'}
-        },
-
-    });
-
-
-    var circle2 = new joint.shapes.devs.CircleModel({
-        position: { x: 310, y: 400 },
-        size: { width: 100, height: 80 },
-        attrs: { circle: { fill: 'white' }, text: { text: 'Circle', fill: 'black' } }
-    });
-
-    rect.on('change:position', function(element) {
-    //    console.log(element.id, ':', element.get('position'));
-    });
-    circle.on('change:position', function(element) {
-      //  console.log(element.id, ':', element.get('position'));
-    });
-
-
-    graph.addCells([rect, rect2]);
-    graph.addCells([circle, circle2]);
-
-    graph.on('change:source change:target', function(link) {
-        if (link.get('source').id && link.get('target').id) {
-            // both ends of the link are connected.
-        }
-    });
-
-
-    paper.on('cell:pointerdblclick', function(cellView, evt, x, y) {
-
+    paper.on('cell:pointerdblclick', function (cellView, evt, x, y) {
         var portsArray = cellView.model.get('outPorts') || [];
         cellView.model.set('outPorts', portsArray.concat([name]), cellView.opt);
-
-
-     //   var portsArray = cellView.model.get('outPorts') || [];
-       // cellView.model.set('outPorts', portsArray.concat([name]), cellView.opt);
-
-                   //  "<div><button class='btn btn-block' id='" + btnDashedId + "'>Dashed</button>" +
-                     //   "<button class='btn btn-block' id='" + btnSolidId + "'>Solid</button></div>";
-
     });
 
 
-    document.getElementById('addRectBtn').onclick= function() {
-        var rec=  new joint.shapes.devs.Model({
-            position: { x: 10, y: 10 },
-            size: { width: 100, height: 80 },
+    function initRectangles() {
+        var rectLeft = new joint.shapes.devs.Model({
+            position: {x: 10, y: 10},
+            size: {width: 80, height: 80},
             attrs: {
-                '.label': { text: 'Model' },
-                rect: { fill: '#2C3E50', rx: 5, ry: 5, 'stroke-width': 2, stroke: 'black'}
+                '.label': {text: 'Rect'}
+            }
+        });
+
+        var rectRight = new joint.shapes.devs.Model({
+            position: {x: 385, y: 10},
+            size: {width: 80, height: 80},
+            attrs: {
+                '.label': {text: 'Rect', 'ref-x': .5, 'ref-y': .2},
+                rect: {rx: 5, ry: 5, stroke: 'black', 'stroke-dasharray': '5'}
+            }
+        });
+
+
+        graph.addCells([rectLeft, rectRight]);
+
+    }
+
+    function initCircles() {
+        joint.shapes.devs.CircleModel = joint.shapes.devs.Model.extend({
+
+            markup: '<g class="rotatable"><g class="scalable"><circle class="body"/></g><text class="label"/><g class="inPorts"/><g class="outPorts"/></g>',
+            portMarkup: '<g class="port port<%= id %>"><rect class="port-body"/><text class="port-label"/></g>',
+
+            defaults: joint.util.deepSupplement({
+                type: 'devs.CircleModel',
+                attrs: {
+                    '.body': {r: 50, cx: 50},
+                    '.label': {text: 'Circle Model', 'ref-y': 0.5, 'y-alignment': 'middle'},
+                    '.port-body': {width: 10, height: 10, x: -5, stroke: 'gray', fill: 'lightgray', magnet: 'active'}
+                }
+
+            }, joint.shapes.devs.Model.prototype.defaults)
+        });
+
+        joint.shapes.devs.CircleModelView = joint.shapes.devs.ModelView;
+        var circleLeft = new joint.shapes.devs.CircleModel({
+            position: {x: 10, y: 440},
+            size: {width: 80, height: 80},
+            attrs: {circle: {fill: 'white'}, text: {text: 'Circle', fill: 'black'}}
+        });
+
+        var circleRight = new joint.shapes.devs.CircleModel({
+            position: {x: 390, y: 440},
+            size: {width: 80, height: 80},
+            attrs: {circle: {fill: 'white'}, text: {text: 'Circle', fill: 'black'}}
+        });
+
+        graph.addCells([circleLeft, circleRight]);
+
+    }
+
+    function addRectangle() {
+        var rect = new joint.shapes.devs.Model({
+            position: {x: 500 / 2, y: 500 / 2},
+            size: {width: 80, height: 80},
+            attrs: {
+                '.label': {text: 'Rect'},
             }
 
         });
-        graph.addCells([rec]);
-    };
+        graph.addCells([rect]);
 
+    }
 
-    document.getElementById('addCircleBtn').onclick= function() {
-
-        var circle2 = new joint.shapes.devs.CircleModel({
-
-            position: { x: 10, y: 10 },
-            size: { width: 100, height: 80 },
-            attrs: { circle: { fill: 'white' }, text: { text: 'Circle', fill: 'black' } }
+    function addCircle() {
+        var circle = new joint.shapes.devs.CircleModel({
+            position: {x: 500 / 2, y: 500 / 2},
+            size: {width: 80, height: 80},
+            '.label': {text: 'Circle'},
+            attrs: {circle: {fill: 'white'}, text: {text: 'Circle', fill: 'black'}}
         });
+        graph.addCells([circle]);
+    }
 
-        graph.addCells([circle2]);
-
-    };
-
-    document.getElementById('exportBtn').onclick= function() {
+    function exportAsImage() {
         var downloadWindow = window.open("Image", "Image from JointJS");
         downloadWindow.document.write('<canvas id="canvas" width="500px" height="500px"></canvas>');
         var svg = document.getElementById("canvas").innerHTML;
-         canvg(downloadWindow.document.getElementById("canvas"), svg);
-         var dataURL = downloadWindow.document.getElementById("canvas").toDataURL();
+        canvg(downloadWindow.document.getElementById("canvas"), svg);
+        var dataURL = downloadWindow.document.getElementById("canvas").toDataURL();
         downloadWindow.location = dataURL;
-};
 
+    }
 
+    function main() {
+        initRectangles();
+        initCircles();
+
+        $("#addRectBtn").click(function () {
+            addRectangle();
+        });
+
+        $("#addCircleBtn").click(function () {
+            addCircle();
+        });
+
+        $("#exportBtn").click(function () {
+            exportAsImage()
+        });
+    }
+
+    main();
 }());
 
