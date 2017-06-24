7d2d20f253b8e9107af5a6658377433283919b85
Franziska Hutter <franziska_hutter@Ok.de>
2017-06-24 19:35:03 +0200

    JointJS
    Grid

--- src/app/jointjs/src/app/performance-jointjs.js
+++ src/app/jointjs/src/app/performance-jointjs.js
@@ -28,41 +28,63 @@
     var rect;
     var l;
     var rectId =0;
+    var recID2 =0;
 
     for (var i = 0; i < items; i++) {
         for (var j = 0; j < items; j++) {
 
-            rectId++;
-
             rect = new joint.shapes.basic.Rect({
-
                 id: 'rect'+rectId,
                 position: {x: pos2, y: pos},
                 size: {width: 100, height: 80},
                 attrs: {
                     '.label': {text: 'Model'}
-
                 }
             });
-
             graph.addCell([rect]);
-            console.log(rect.id);
 
             if(j >=1 && j <= (items-1)) {
                  l = new joint.dia.Link({
                     source: {id: 'rect'+(rectId-1)},
-                    target: {id: 'rect'+rectId},
-                    attrs: {
-                        '.connection': {stroke: 'black'}
-                    },
+                    target: {id: 'rect'+rectId}
 
                 });
                 graph.addCell(l);
             }
+            rectId++;
             pos2 =  pos2 + 150;
         }
-        graph.addCell([rect]);
+
         pos2 = 10;
         pos = pos +150;
+    }
+
+
+    for (var z = 0; z <= items; z++) {
+        for (var h = 0; h < items; h++) {
+            if(z>1) {
+
+                var rec1 =((rectId % items));
+                var rec2 =(((rectId % items )+ items));
+
+                if(recID2>= 5 && z > 2)
+                {
+                    rec1 =rec1 + recID2;
+                    rec2 = rec2+recID2;
+                }
+                l = new joint.dia.Link({
+                    source: {id: 'rect' +( rec1)},
+                    target: {id: 'rect' + (rec2)}
+
+                });
+
+                console.log(rec1 + '    '+ rec2);
+                graph.addCell(l);
+            }
+            rectId++;
+        }
+        if(z>1) {
+            recID2 = recID2 + items;
+        }
     }
 }());
