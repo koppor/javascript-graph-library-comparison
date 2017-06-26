4281007d6289460b6fc34e7f4eebfc85fd1c5999
Alex Frank <alex_frank@email.com>
2017-06-26 11:51:48 +0200

    Added basic layout #BPMN4T-35

--- src/app/jsplumb/src/app/jsplumb-demo.js
+++ src/app/jsplumb/src/app/jsplumb-demo.js
@@ -3,6 +3,8 @@
 
     jsPlumb.ready(function () {
 
+        jsPlumb.setContainer("container");
+
         function exportAsImage() {
             alert("export");
         }
@@ -16,11 +18,32 @@
         }
 
         function initRectangles() {
+            var rectLeftId = "rectLeft";
+            var rectRightId = "rectRight";
+
+            jsPlumb.draggable([rectLeftId, rectRightId], {
+                containment: true
+            });
+
+            jsPlumb.connect({
+                source: rectLeftId,
+                target: rectRightId
+            });
 
         }
 
         function initCircles() {
+            var circleLeftId = "circleLeft";
+            var circleRightId = "circleRight";
 
+            jsPlumb.draggable([circleLeftId, circleRightId], {
+                containment: true
+            });
+
+            jsPlumb.connect({
+                source: circleLeftId,
+                target: circleRightId
+            });
         }
 
         function main() {
