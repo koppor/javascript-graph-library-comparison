e4b41623d2a56d8e23ff2f4c915cc94892c7d644
Franziska Hutter <franziska_hutter@Ok.de>
2017-06-28 11:45:10 +0200

    JointJS Performance
    Perdormancen JointJS

--- src/app/jointjs/src/app/performance-jointjs.js
+++ src/app/jointjs/src/app/performance-jointjs.js
@@ -4,87 +4,89 @@
 (function () {
     "use strict";
 
-    var canvas2 = $('#canvas2');
+    var canvas2 = $('#jointjs');
     var graph = new joint.dia.Graph();
 
 
     var paper = new joint.dia.Paper({
         el: canvas2,
-        width: 50000,
-        height: 50000,
-        model: graph,
-        defaultLink: new joint.dia.Link({
-            attrs: {
-                '.marker-target': {d: 'M 10 0 L 0 5 L 10 10 z'},
-                text: 'label'
-            }
-        })
+        width: canvas2.width(),
+        height: canvas2.height(),
+        model: graph
     });
 
+    function generateGrid() {
 
-    var items = 5;
-    var pos = 50;
-    var pos2 = 10;
-    var rect;
-    var l;
-    var rectId =0;
-    var recID2 =0;
+        var y = parseInt($("#yInput").val(), 10);
+        var x = parseInt($("#xInput").val(), 10);
+        var pos = 50;
+        var pos2 = 10;
+        var rect;
+        var l;
+        var rectId = 0;
+        var recID2 = 0;
 
-    for (var i = 0; i < items; i++) {
-        for (var j = 0; j < items; j++) {
+        for (var i = 0; i < y; i++) {
+            for (var j = 0; j < x; j++) {
 
-            rect = new joint.shapes.basic.Rect({
-                id: 'rect'+rectId,
-                position: {x: pos2, y: pos},
-                size: {width: 100, height: 80},
-                attrs: {
-                    '.label': {text: 'Model'}
-                }
-            });
-            graph.addCell([rect]);
-
-            if(j >=1 && j <= (items-1)) {
-                 l = new joint.dia.Link({
-                    source: {id: 'rect'+(rectId-1)},
-                    target: {id: 'rect'+rectId}
-
+                rect = new joint.shapes.basic.Rect({
+                    id: 'rect' + rectId,
+                    position: {x: pos2, y: pos},
+                    size: {width: 100, height: 80},
+                    attrs: {
+                        '.label': {text: 'Model'}
+                    }
                 });
-                graph.addCell(l);
+                graph.addCell([rect]);
+
+                if (j >= 1 && j <= (y - 1)) {
+                    l = new joint.dia.Link({
+                        source: {id: 'rect' + (rectId - 1)},
+                        target: {id: 'rect' + rectId}
+
+                    });
+                    graph.addCell(l);
+                }
+                rectId++;
+                pos2 = pos2 + 150;
             }
-            rectId++;
-            pos2 =  pos2 + 150;
+
+            pos2 = 10;
+            pos = pos + 150;
         }
 
-        pos2 = 10;
-        pos = pos +150;
+
+        for (var z = 0; z <= y; z++) {
+            for (var h = 0; h < x; h++) {
+                if (z > 1) {
+
+                    var rec1 = ((rectId % x));
+                    var rec2 = (((rectId % x ) + x));
+
+                    if (recID2 >= 5 && z > 2) {
+                        rec1 = rec1 + recID2;
+                        rec2 = rec2 + recID2;
+                    }
+                    l = new joint.dia.Link({
+                        source: {id: 'rect' + ( rec1)},
+                        target: {id: 'rect' + (rec2)}
+
+                    });
+
+                    console.log(rec1 + '    ' + rec2);
+                    graph.addCell(l);
+                }
+                rectId++;
+            }
+            if (z > 1) {
+                recID2 = recID2 + x;
+            }
+        }
     }
 
-
-    for (var z = 0; z <= items; z++) {
-        for (var h = 0; h < items; h++) {
-            if(z>1) {
-
-                var rec1 =((rectId % items));
-                var rec2 =(((rectId % items )+ items));
-
-                if(recID2>= 5 && z > 2)
-                {
-                    rec1 =rec1 + recID2;
-                    rec2 = rec2+recID2;
-                }
-                l = new joint.dia.Link({
-                    source: {id: 'rect' +( rec1)},
-                    target: {id: 'rect' + (rec2)}
-
-                });
-
-                console.log(rec1 + '    '+ rec2);
-                graph.addCell(l);
-            }
-            rectId++;
-        }
-        if(z>1) {
-            recID2 = recID2 + items;
-        }
+    function main() {
+        $("#generateBtn").click(generateGrid);
     }
+    main();
+
 }());
