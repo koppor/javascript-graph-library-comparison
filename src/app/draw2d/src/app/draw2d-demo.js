a802ac19196cf73a1e2286ccca8fe4e92f3a58ef
Birasanth Pushpanathan <birasanth.push@hotmail.de>
2017-06-30 13:28:39 +0200

    rect and circle

--- src/app/draw2d/src/app/draw2d-demo.js
+++ src/app/draw2d/src/app/draw2d-demo.js
@@ -1,19 +1,14 @@
-
-
-$(window).load(function () {
+(function () {
+    "use strict";
 
 
     var canvas = new draw2d.Canvas("canvas");
 
 
-    canvas.add(new draw2d.shape.node.Start(), 20, 10, 80, 80);
 
+    canvas.add(new draw2d.shape.basic.Rectangle({width: 80, height: 80, x: 5, y: 10, keepAspectRatio: true}));
+    canvas.add(new draw2d.shape.basic.Rectangle({width: 80, height: 80, x: 380, y: 10, keepAspectRatio: true}));
+    canvas.add(new draw2d.shape.basic.Circle({radius: 40, x: 5, y: 400}));
+    canvas.add(new draw2d.shape.basic.Circle({radius: 40, x: 380, y: 400}));
 
-    canvas.add(new draw2d.shape.node.End(),400, 10, 80, 80);
-
-
-    var shape =  new draw2d.shape.basic.Circle({x:40,y:10, stroke:3});
-
-    //canvas.add(shape);
-
-});
+}());
