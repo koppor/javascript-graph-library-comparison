453febb4c3228dc15619380a69e95f08046e1e09
Birasanth Pushpanathan <birasanth.push@hotmail.de>
2017-06-30 13:03:12 +0200

    change draw2d

--- src/app/draw2d/src/app/draw2d-demo.js
+++ src/app/draw2d/src/app/draw2d-demo.js
@@ -1,15 +1,19 @@
+
+
 $(window).load(function () {
 
 
-            var canvas = new draw2d.Canvas("canvas");
+    var canvas = new draw2d.Canvas("canvas");
 
-            canvas.add(new draw2d.shape.basic.Oval(), 100, 100);
-            canvas.add(new draw2d.shape.basic.Rectangle(), 120, 150);
 
-            canvas.add(new draw2d.shape.node.Start(), 80, 80);
-            canvas.add(new draw2d.shape.node.Start(), 50, 50);
+    canvas.add(new draw2d.shape.node.Start(), 20, 10, 80, 80);
 
-            canvas.add(new draw2d.shape.node.End(), 150, 150);
-            canvas.add(new draw2d.shape.node.End(), 350, 250);
 
-        });
+    canvas.add(new draw2d.shape.node.End(),400, 10, 80, 80);
+
+
+    var shape =  new draw2d.shape.basic.Circle({x:40,y:10, stroke:3});
+
+    //canvas.add(shape);
+
+});
