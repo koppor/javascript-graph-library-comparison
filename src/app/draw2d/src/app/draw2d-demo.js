554eb15fcc9397c60de31e97eaf335c56f78b40b
Birasanth Pushpanathan <birasanth.push@hotmail.de>
2017-07-03 15:37:17 +0200

    change Border/ arrow in demo.js

--- src/app/draw2d/src/app/draw2d-demo.js
+++ src/app/draw2d/src/app/draw2d-demo.js
@@ -17,34 +17,40 @@
         height: 80,
         bgColor: 'lightgrey'
     });
-    var rect2 = new draw2d.shape.basic.Rectangle({
+    rect1.add(new draw2d.shape.basic.Label({text:"Rect"}),  new draw2d.layout.locator.CenterLocator());
+    rect1.createPort("input", new draw2d.layout.locator.LeftLocator(rect1));
+    canvas.add(rect1);
+    rect1.installEditPolicy(new draw2d.policy.figure.GlowSelectionFeedbackPolicy());
+
+
+        var rect2 = new draw2d.shape.basic.Rectangle({
         x: 5,
         y: 10,
         width: 80,
         height: 80,
         bgColor: 'lightgrey'
     });
-    rect1.add(new draw2d.shape.basic.Label({text:"Rect"}),  new draw2d.layout.locator.CenterLocator());
     rect2.add(new draw2d.shape.basic.Label({text:"Rect"}),  new draw2d.layout.locator.CenterLocator());
-
-    rect1.createPort("input", new draw2d.layout.locator.LeftLocator(rect1));
-    canvas.add(rect1);
     rect2.createPort("output", new draw2d.layout.locator.RightLocator(rect2));
     canvas.add(rect2);
+    rect2.installEditPolicy(new draw2d.policy.figure.GlowSelectionFeedbackPolicy());
 
+
+        var label = new draw2d.shape.basic.Label({text:"Label"});
 
      c = new draw2d.Connection({
         source: rect2.getOutputPort(0),
         target: rect1.getInputPort(0),
         router: new draw2d.layout.connection.InteractiveManhattanConnectionRouter()
     });
+       c.add(label, new draw2d.layout.locator.ManhattanMidpointLocator());
 
 
 // you can set the vertices of the connection via API. But normally this is
 // happen by the user or by reading the JSON file via a draw2d.io.Reader.
 // .... anyway - it is possible via API too.
 //
-    c.setVertices([{
+    /*c.setVertices([{
         x: 350,
         y: 75
     }, {
@@ -52,7 +58,7 @@
         y: 175
     }]);
 
-
+*/
     }
     function initCircle() {
 
@@ -60,6 +66,7 @@
         leftCircle.setColor("#000000");
         leftCircle.setBackgroundColor("#FFFFFF");
         leftCircle.add(new draw2d.shape.basic.Label({text:"Circle"}), new draw2d.layout.locator.CenterLocator());
+        leftCircle.installEditPolicy(new draw2d.policy.figure.GlowSelectionFeedbackPolicy());
         canvas.add( leftCircle, 5,400);
 
 
@@ -67,6 +74,7 @@
         rightCircle.setColor("#000000");
         rightCircle.setBackgroundColor("#FFFFFF");
         rightCircle.add(new draw2d.shape.basic.Label({text:"Circle"}).setColor("#FFFFFF"), new draw2d.layout.locator.CenterLocator());
+        rightCircle.installEditPolicy(new draw2d.policy.figure.GlowSelectionFeedbackPolicy());
         canvas.add( rightCircle, 380,400);
 
 
@@ -83,14 +91,6 @@
         });
 
 
-        c.setVertices([{
-            x: 280,
-            y: 75
-        }, {
-            x: 150,
-            y: 175
-        }]);
-
     }
 
 
@@ -103,13 +103,20 @@
         downloadWindow.location = dataURL;
     }
 
+        
+    function createNewRect() {
+
+        canvas.add(new draw2d.shape.node.Between({width: 80, height: 80, x: 200, y: 200,}));
+        canvas.installEditPolicy(new draw2d.policy.canvas.CoronaDecorationPolicy());
+
+    }
 
     function main() {
         initRectangles();
         initCircle();
         $("#addRectBtn").click(function () {
-
-            canvas.add(new draw2d.shape.basic.Rectangle({width: 80, height: 80, x: 200, y: 200, keepAspectRatio: true}));
+            createNewRect();
+           // canvas.add(new draw2d.shape.basic.Rectangle({width: 80, height: 80, x: 200, y: 200, keepAspectRatio: true}));
 
         });
         $("#addCircleBtn").click(function () {
